f435bde2d19d47b2503f3646d2ea50e261994674
toritree <65443082+toritree@users.noreply.github.com>
2022-02-26 19:22:25 +0900

    add grid

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import {Render} from "./render"
 
 ReactDOM.render(
-    <Render onClickPoint={(x,y)=>{console.log(x,y)}} lattice={100}/>,
+    <Render onClickPoint={(x,y)=>{console.log(x,y)}} lattice={50}/>,
     document.getElementById("root")
 )
--- src/render.tsx
+++ src/render.tsx
@@ -7,23 +7,53 @@ interface renderProps{
 
 export class Render extends React.Component<renderProps>{
     private canvas: React.RefObject<HTMLCanvasElement>
+    private ctx?: CanvasRenderingContext2D
     constructor(props: renderProps){
         super(props)
         this.canvas = React.createRef()
+        this.ctx = undefined
     }
 
     private click = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>)=>{
         const lattice = (v:number) => v % this.props.lattice < this.props.lattice
-            ? v % this.props.lattice
-            : v % this.props.lattice + 1
+            ? Math.round(v / this.props.lattice)
+            : Math.round(v / this.props.lattice + 1)
         this.props.onClickPoint(
             lattice(event.nativeEvent.offsetX),
             lattice(event.nativeEvent.offsetY)
         )
     }
 
+    //set ctx
+    private setCtx = ()=>{
+        const ctx = this.canvas.current?.getContext("2d")
+        if(ctx == null) return
+        this.ctx = ctx
+    }
+
+    //grid
+    private line = ()=>{
+        //type test
+        this.setCtx()
+        const lattice = this.props.lattice
+        if(typeof this.ctx == "undefined" || this.canvas.current == null) return
+        const cv = this.canvas.current
+        //main code
+        this.ctx.strokeStyle = "gray"
+        this.ctx.lineWidth = 1
+        for(let i = 0; i!=Math.floor(cv.width / lattice);i++){
+            this.ctx.strokeRect(i*lattice, 0, lattice, cv.width)
+        }
+        for(let i = 0; i!=Math.floor(cv.height / lattice);i++){
+            this.ctx.strokeRect(0, i*lattice, cv.width, lattice)
+        }
+    }
+
+    componentDidMount = ()=>{
+        this.line()
+    }
+
     render(): React.ReactNode {
-        this.props
         return <canvas
             width={1000}
             height={1000}

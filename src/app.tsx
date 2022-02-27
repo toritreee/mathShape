4e2e974715182666f12f8cefe9e1e36ba4dcf99c
toritree <65443082+toritree@users.noreply.github.com>
2022-02-27 10:06:04 +0900

    最初の線を追加

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 import {Render} from "./render"
 
 ReactDOM.render(
-    <Render onClickPoint={(x,y)=>{console.log(x,y)}} lattice={50} pins={[]}/>,
+    <Render onClickPoint={(x,y)=>{console.log(x,y)}} lattice={50} pins={[
+        {x:2,y:2,name:"A"},
+        {x:4,y:5,name:"B"},
+        {x:5,y:2,name:"C"}
+    ]} lines={[
+        {from:"A",to:"B",type:"segment"}
+    ]}/>,
     document.getElementById("root")
 )
